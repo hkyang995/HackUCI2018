df6aa27459d5c5bd04a94275064db59d7210049f
Henry K. Yang <hkyang@csu.fullerton.edu>
2018-02-03 01:12:08 -0800

    Calendar installed. Can be shown with a button

---
+++ script.js
@@ -0,0 +1,44 @@
+angular.module('mwl.calendar.docs', ['mwl.calendar', 'ngAnimate', 'ui.bootstrap', 'colorpicker.module']);
+angular
+  .module('mwl.calendar.docs')
+  .controller('EditableDeletableEventsCtrl', function(moment, alert, calendarConfig) {
+    
+    var vm = this;
+    vm.events = [
+      {
+        title: 'Editable event',
+        color: calendarConfig.colorTypes.warning,
+        startsAt: moment().startOf('month').toDate(),
+        actions: [{
+          label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
+          onClick: function(args) {
+            alert.show('Edited', args.calendarEvent);
+          }
+        }]
+      }, {
+        title: 'Deletable event',
+        color: calendarConfig.colorTypes.info,
+        startsAt: moment().startOf('month').toDate(),
+        actions: [{
+          label: '<i class=\'glyphicon glyphicon-remove\'></i>',
+          onClick: function(args) {
+            alert.show('Deleted', args.calendarEvent);
+          }
+        }]
+      }, {
+        title: 'Non editable and deletable event',
+        color: calendarConfig.colorTypes.important,
+        startsAt: moment().startOf('month').toDate()
+      }
+    ];
+
+    vm.calendarView = 'month';
+    vm.viewDate = moment().startOf('month').toDate();
+    vm.cellIsOpen = true;
+
+  })
+  .controller('btnCtrl', ['$scope', function($scope){
+    $scope.showCal = function(){
+      document.getElementById("calendar_wrapper").style.display="inline";
+    }
+  }]);
