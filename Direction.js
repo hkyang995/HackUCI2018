f987660fc6f535b166e15cbfbadb911529b04d75
Joey <joeyliao@outlook.com>
2018-02-04 01:35:09 -0800

    Bunch of API stuff but some issues
    issues:
    -inconsistency with GET requests; cannot call w/o declines
    -variables are not changing sometimes

---
+++ Direction.js
@@ -0,0 +1,56 @@
+//calculation for miles shit
+function sortEvents(){
+	// // should be this: getTargetInfo(eventObject[0].title);
+	setTimeout(function(){point1Info(eventObject[0]);},3000);
+	point1 = addressFormat1; //the immediate created placeID is stored in vacationHome
+	setTimeout(function(){point2Info(eventObject[1]);},3000);
+	point2 = addressFormat2;
+	setTimeout(function(){twoPointInfo(point1, point2);}, 3000);
+	
+	//problem: inconsistent GET request
+
+
+	// // point1Info(eventObject[0]);
+	// // setTimeout(function(){point1 = addressFormat1;}, 3000); //the immediate created placeID is stored in vacationHome
+	// point2Info(eventObject[2]);
+	// point2 = addressFormat2;
+	// twoPointInfo(point1, point2);
+
+
+	// // point1Info(eventObject[0]);
+	// // setTimeout(function(){point1 = addressFormat1;}, 3000); //the immediate created placeID is stored in vacationHome
+	// point2Info(eventObject[3]);
+	// setTimeout(function(){point2 = addressFormat2;}, 3000);
+	// setTimeout(function(){twoPointInfo(point1, point2);},3000);
+
+	// for(var i = 0; i < 1; i++){
+	// 	point1Info(eventObject[i]);
+	// 	setTimeout(function(){point1 = addressFormat1;}, 3000);
+	// 	for(var j = 1; j < 4; j++){
+	// 		 //the immediate created placeID is stored in vacationHome
+	// 		point2Info(eventObject[j]);
+	// 		setTimeout(function(){point2 = addressFormat2;}, 3000);
+	// 		setTimeout(function(){}, 3000);
+	// 		setTimeout(function(){twoPointInfo(point1, point2);},3000);
+	// 	}
+		
+	// }
+
+
+
+
+
+}
+
+function twoPointInfo(point1, point2){
+	fetch("https://proxy-sauce.glitch.me/https://maps.googleapis.com/maps/api/directions/json?\
+		key=AIzaSyACC7GudsQruzi7LLyFrLRq78HnjJvJBlU&\
+		mode=driving&\
+		destination=" + point1 + "&origin=" + point2 + "\"")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      console.log(data.routes[0].legs[0].distance.text);
+      console.log(data.routes[0].legs[0].duration.text);
+	});
+}
--- PlaceSearch.js
+++ PlaceSearch.js
@@ -1,12 +1,20 @@
 
 //How to use:
 // -getLatLong(userDest);
-// -initialize(lat, lng);
+// -newMap(lat, lng);
+// -optional: searchMarkers(lat, lng);
 
 
 //global variables for data storing, etc
 var lat = "";
 var lng = "";
+var addressFormat1 = "";
+var addressFormat2 = "";
+var destMap = "";
+var eventObject = ["2952 Maxson Road El Monte", "19505 Galeview Drive Rowland Heights", "diamond bar high school", "the shoppes"];
+var point1 = "";
+var point2 = "";
+
 
 function initMap() {
     // Create a map object and specify the DOM element for display.
@@ -16,35 +24,41 @@ function initMap() {
       });
 
     infoWindow = new google.maps.InfoWindow;
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-        };
 
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
-        infoWindow.open(map);
-        map.setCenter(pos);
-      }, function() {
-          handleLocationError(true, infoWindow, map.getCenter());
-      });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
+    // // HTML5 geolocation.
+    // if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition(function(position) {
+    //     var pos = {
+    //         lat: position.coords.latitude,
+    //         lng: position.coords.longitude
+    //     };
+
+    //     infoWindow.setPosition(pos);
+    //     infoWindow.setContent('Location found.');
+    //     infoWindow.open(map);
+    //     map.setCenter(pos);
+    //   }, function() {
+    //       handleLocationError(true, infoWindow, map.getCenter());
+    //   });
+    // } else {
+    //     // Browser doesn't support Geolocation
+    //     handleLocationError(false, infoWindow, map.getCenter());
+    // }
 }
 
-//takes user to destination + searches for nearby restaurants
-function initialize(destLat , destLng) {
-  var map = new google.maps.Map(document.getElementById('map'), {
+//takes user to destination 
+function newMap(destLat , destLng) {
+    destMap = new google.maps.Map(document.getElementById('map'), {
     center: {lat: destLat, lng: destLng},
     zoom: 15,
     scrollwheel: false
   });
 
+  //optional: searchMarkers();
+}
+
+//searches for nearby restaurants
+function searchMarkers(destMap, destLat , destLng){
   // Specify location, radius and place types for your Places API search.
   var request = {
     location: {lat: destLat, lng: destLng},
@@ -54,7 +68,7 @@ function initialize(destLat , destLng) {
 
   // Create the PlaceService and send the request.
   // Handle the callback with an anonymous function.
-  var service = new google.maps.places.PlacesService(map);
+  var service = new google.maps.places.PlacesService(destMap);
   service.nearbySearch(request, function(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
       for (var i = 0; i < results.length; i++) {
@@ -63,7 +77,7 @@ function initialize(destLat , destLng) {
         // the map as a marker, and register an event to handle a
         // click on the marker.
         var marker = new google.maps.Marker({
-          map: map,
+          map: destMap,
           position: place.geometry.location
         });
       }
@@ -71,10 +85,27 @@ function initialize(destLat , destLng) {
   });
 }
 
+// // get Lat and Long of destination 
+// function getLatLong(userDest){
+//   var destination = userDest;
+//   axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+//     params:{
+//       address: destination,
+//       key: 'AIzaSyACC7GudsQruzi7LLyFrLRq78HnjJvJBlU'
+//     }
+//   })
+//   .then(function(response){
+//     console.log(response);
+//     lat = response.data.results[0].geometry.location.lat;
+//     lng = response.data.results[0].geometry.location.lng;
+//   })
+//   .catch(function(error){
+//     console.log(error);
+//   });
+// }
 
-//get destination of Lat and Long
-function getLatLong(userDest){
-  //temportarily hard coded. replace w/ user input from form
+//get info of destination (Lat, Long, address)
+function getTargetInfo(userDest){
   var destination = userDest;
   axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
     params:{
@@ -86,8 +117,51 @@ function getLatLong(userDest){
     console.log(response);
     lat = response.data.results[0].geometry.location.lat;
     lng = response.data.results[0].geometry.location.lng;
+    addressFormat = response.data.results[0].formatted_address;
   })
   .catch(function(error){
     console.log(error);
   });
 }
+
+//get info of point1 (address)
+function point1Info(userDest){
+  var destination = userDest;
+  axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+    params:{
+      address: destination,
+      key: 'AIzaSyACC7GudsQruzi7LLyFrLRq78HnjJvJBlU'
+    }
+  })
+  .then(function(response){
+    console.log(response);
+    addressFormat1 = response.data.results[0].formatted_address;
+  })
+  .catch(function(error){
+    console.log(error);
+  });
+}
+
+//get info of point2 (address)
+function point2Info(userDest){
+  var destination = userDest;
+  axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+    params:{
+      address: destination,
+      key: 'AIzaSyACC7GudsQruzi7LLyFrLRq78HnjJvJBlU'
+    }
+  })
+  .then(function(response){
+    console.log(response);
+    addressFormat2 = response.data.results[0].formatted_address;
+  })
+  .catch(function(error){
+    console.log(error);
+  });
+}
+
+
+
+
+
+
